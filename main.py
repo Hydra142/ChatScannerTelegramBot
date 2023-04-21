2d77703d76562198e31f0eef86ccf7c38aa42e44
Misha Redchych <mychailo.redchych@lambda-dev.com>
2023-04-21 16:58:45 +0300

    polishing

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from telebot import types
 import sqlite3
 import os
 import tempfile
-TOKEN = "6241338600:AAGl-8YywU5QI7W8Ky-MleqaSrRldqYQrcI"
-PASSWORD = '1111'
+
+TOKEN = os.environ.get('BOT_TOKEN')
+PASSWORD = os.environ.get('PASSWORD')
 bot = telebot.TeleBot(TOKEN)
+
 
 class DbConnector:
     def __init__(self, db_name):
@@ -54,37 +56,49 @@ class DbConnector:
         conn.commit()
         conn.close()
 
+
 db = DbConnector("scanner_bot.db")
 forbidden_message_action = "Delete"
+
 
 def check_password(message):
     return message.text == PASSWORD
 
+
 def is_private_chat(chat):
     return chat.type == 'private'
 
+
 def handle_password(message):
     if check_password(message):
-        bot.send_message(message.chat.id, "Access granted. You can now manage the bot.", reply_markup=get_user_menu_markup())
+        bot.send_message(message.chat.id, "Access granted. You can now manage the bot.",
+                         reply_markup=get_user_menu_markup())
     else:
         bot.send_message(message.chat.id, "Access denied. Invalid password.")
+
 
 def handle_show_chats(message):
     chat_list = db.read("SELECT chat_id, name, is_monitoring_enabled FROM chats")
     if chat_list:
-        formatted_list = '\n'.join([f"Chat ID: {chat[0]}, Name: {chat[1]}, Monitoring: {'Enabled' if chat[2] else 'Disabled'}" for chat in chat_list])
+        formatted_list = '\n'.join(
+            [f"Chat ID: {chat[0]}, Name: {chat[1]}, Monitoring: {'Enabled' if chat[2] else 'Disabled'}" for chat in
+             chat_list])
         bot.send_message(message.chat.id, f"Chats:\n{formatted_list}")
     else:
         bot.send_message(message.chat.id, "No chats found.")
 
+
 def handle_delete_blacklist_word(message):
     word_to_delete = message.text
     db.write("DELETE FROM messages_black_list WHERE message=?", (word_to_delete,))
-    bot.send_message(message.chat.id, f"Deleted word '{word_to_delete}' from the blacklist.", reply_markup=get_user_menu_markup())
+    bot.send_message(message.chat.id, f"Deleted word '{word_to_delete}' from the blacklist.",
+                     reply_markup=get_user_menu_markup())
+
 
 def handle_delete_messages_history(message):
     db.write("DELETE FROM messages_history")
     bot.send_message(message.chat.id, "Deleted all messages history.")
+
 
 def handle_add_blacklist_word(message):
     word = message.text
@@ -94,6 +108,7 @@ def handle_add_blacklist_word(message):
     except sqlite3.IntegrityError:
         bot.reply_to(message, f"'{word}' is already in the blacklist.", reply_markup=get_user_menu_markup())
 
+
 def handle_show_blacklist(message):
     blacklist = db.read("SELECT message FROM messages_black_list")
     if blacklist:
@@ -102,10 +117,12 @@ def handle_show_blacklist(message):
     else:
         bot.send_message(message.chat.id, "The blacklist is empty.")
 
+
 def handle_enable_monitoring(message):
     chat_id = message.text
     db.write("UPDATE chats SET is_monitoring_enabled = 1 WHERE chat_id = ?", (chat_id,))
     bot.reply_to(message, f"Monitoring enabled for chat ID {chat_id}.")
+
 
 def handle_disable_monitoring(message):
     chat_id = message.text
@@ -119,10 +136,12 @@ def add_blacklist_word(message):
         bot.reply_to(message, "Enter the word you want to add to the blacklist:")
         bot.register_next_step_handler(message, handle_add_blacklist_word)
 
+
 @bot.message_handler(commands=['show_blacklist'])
 def show_blacklist(message):
     if is_private_chat(message.chat) and check_password(message):
         handle_show_blacklist(message)
+
 
 @bot.message_handler(commands=['enable_monitoring'])
 def enable_monitoring(message):
@@ -130,15 +149,18 @@ def enable_monitoring(message):
         bot.reply_to(message, "Enter the chat ID to enable monitoring:")
         bot.register_next_step_handler(message, handle_enable_monitoring)
 
+
 @bot.message_handler(commands=['disable_monitoring'])
 def disable_monitoring(message):
     if is_private_chat(message.chat) and check_password(message):
         bot.reply_to(message, "Enter the chat ID to disable monitoring:")
         bot.register_next_step_handler(message, handle_disable_monitoring)
 
+
 def remove_special_chars_and_spaces(input_string):
     cleaned_string = ''.join([char for char in input_string if char.isalnum()])
     return cleaned_string
+
 
 # def generate_substrings(input_string):
 #     cleaned_string = remove_special_chars_and_spaces(input_string)
@@ -160,6 +182,7 @@ def add_to_messages_history(chat_name, user_name, message_text, sent_datetime, m
                 (chat_name, user_name, message_text, sent_datetime, matched_forbidden_words)
                 VALUES (?, ?, ?, ?, ?)''', (chat_name, user_name, message_text, sent_datetime, matched_forbidden_words))
 
+
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     chat = message.chat
@@ -177,11 +200,13 @@ def get_user_menu_markup():
 
     return markup
 
+
 def get_chat_list_markup(chat_list, prefix):
     markup = types.InlineKeyboardMarkup()
     for chat in chat_list:
         markup.add(types.InlineKeyboardButton(chat[1], callback_data=f"{prefix}:{chat[0]}"))
     return markup
+
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('enable') or call.data.startswith('disable'))
 def handle_callback_query(call):
@@ -195,6 +220,7 @@ def handle_callback_query(call):
         db.write("UPDATE chats SET is_monitoring_enabled=0 WHERE chat_id=?", (chat_id,))
         bot.answer_callback_query(call.id, f"Monitoring disabled for chat {chat_id}.")
 
+
 def handle_enable_monitoring(message):
     chat_list = db.read("SELECT chat_id, name FROM chats WHERE is_monitoring_enabled=0")
     if chat_list:
@@ -202,6 +228,7 @@ def handle_enable_monitoring(message):
         bot.send_message(message.chat.id, "Select a chat to enable monitoring:", reply_markup=markup)
     else:
         bot.send_message(message.chat.id, "No chats with disabled monitoring were found.")
+
 
 def handle_disable_monitoring(message):
     chat_list = db.read("SELECT chat_id, name FROM chats WHERE is_monitoring_enabled=1")
@@ -211,37 +238,41 @@ def handle_disable_monitoring(message):
     else:
         bot.send_message(message.chat.id, "No chats with enabled monitoring were found.")
 
+
 def handle_show_messages_history(message):
     messages_history = db.read("SELECT * FROM messages_history")
     if messages_history:
 
+        chat_history_text = ""
+        for count, history in enumerate(messages_history, start=1):
+            chat_history_text += (f"{count}) Chat Name: {history[1]}\n"
+                                  f"   User: @{history[2]}\n"
+                                  f"   Message: {history[3]}\n"
+                                  f"   Sent: {history[4]}\n"
+                                  f"   Forbidden Words: {history[5]}\n\n")
+        try:
+            bot.send_message(message.chat.id, chat_history_text)
+        except:
+            bot.send_message(message.chat.id, "History is too long!")
 
-            chat_history_text = ""
-            for count, history in enumerate(messages_history, start=1):
-                chat_history_text += (f"{count}) Chat Name: {history[1]}\n"
-                                      f"   User: @{history[2]}\n"
-                                      f"   Message: {history[3]}\n"
-                                      f"   Sent: {history[4]}\n"
-                                      f"   Forbidden Words: {history[5]}\n\n")
-            try:
-                bot.send_message(message.chat.id, chat_history_text)
-            except:
-                bot.send_message(message.chat.id, "History is too long!")
+        # Save the history text to a file.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False) as temp_file:
+            temp_file.write(chat_history_text)
+            temp_file.flush()
+            temp_file.close()
 
-            # Save the history text to a file.
-            with tempfile.NamedTemporaryFile(mode="w+", delete=False) as temp_file:
-                temp_file.write(chat_history_text)
-                temp_file.flush()
-                temp_file.close()
+            with open(temp_file.name, "rb") as file:
+                bot.send_document(message.chat.id, file, caption="here's your history")
 
-                with open(temp_file.name, "rb") as file:
-                    bot.send_document(message.chat.id, file, caption="here's your history")
-
-            os.unlink(temp_file.name)
+        os.unlink(temp_file.name)
     else:
         bot.send_message(message.chat.id, "No messages history found.")
 
-@bot.message_handler(func=lambda m: m.text in ["Add to blacklist", "Delete blacklist word", "Show blacklist", "Show chats", "Show messages history", "Delete all messages history", "Enable monitoring", "Disable monitoring", "Action:Delete", "Action:Warning"] and is_private_chat(m.chat))
+
+@bot.message_handler(
+    func=lambda m: m.text in ["Add to blacklist", "Delete blacklist word", "Show blacklist", "Show chats",
+                              "Show messages history", "Delete all messages history", "Enable monitoring",
+                              "Disable monitoring", "Action:Delete", "Action:Warning"] and is_private_chat(m.chat))
 def handle_menu_options(message):
     chat_id = message.chat.id
     if message.text == "Add to blacklist":
@@ -275,7 +306,9 @@ def handle_menu_options(message):
     elif message.text in ("Action:Delete", "Action:Warning"):
         global forbidden_message_action
         forbidden_message_action = "Delete" if message.text.split(':')[1] == "Warning" else "Warning"
-        bot.reply_to(message, f"Changed forbidden word usage action to {forbidden_message_action}", reply_markup=get_user_menu_markup())
+        bot.reply_to(message, f"Changed forbidden word usage action to {forbidden_message_action}",
+                     reply_markup=get_user_menu_markup())
+
 
 @bot.message_handler(func=lambda m: True)
 def handle_messages(message):
@@ -306,10 +339,10 @@ def handle_messages(message):
                 global forbidden_message_action
                 if forbidden_message_action == "Delete":
                     bot.delete_message(chat_id, message.message_id)
-                    bot.send_message(chat_id,f"@{user_name} sent a message containing forbidden words: {matched_forbidden_words}")
+                    bot.send_message(chat_id,
+                                     f"@{user_name} sent a message containing forbidden words: {matched_forbidden_words}")
                 else:
                     bot.reply_to(message, f"You've sent forbidden words: {matched_forbidden_words}")
 
-# Start the bot
 
 bot.polling(none_stop=True)
